bd699defa62643e7f00a742df62226a46df13e89
SALEM <salemnk02@gmail.com>
2023-12-18 11:40:54 +0100

    Display all items

--- pieces.js
+++ pieces.js
@@ -1,32 +1,31 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
-console.log(pieces)
+// console.log(pieces)
+
+for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i];
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie && "(aucune catégorie)";
+
+    const descriptionItem = document.createElement("p");
+    descriptionItem.innerText = article.description && "(Pas de description pour le moment.)"
+
+    const availableItem = document.createElement("p");
+    availableItem.innerText = article.disponibilite === "oui" ? "En stock" : "Rupture de stock"
 
 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie && "(aucune catégorie)";
-
-const descriptionItem = document.createElement("p");
-descriptionItem.innerText = article.description && "(Pas de description pour le moment.)"
-
-const availableItem = document.createElement("p");
-availableItem.innerText = article.disponibilite === "oui" ? "En stock" : "Rupture de stock"
-
-
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionItem);        
-sectionFiches.appendChild(availableItem);      
-
-    
+    const sectionFiches = document.querySelector(".fiches");
+    sectionFiches.appendChild(imageElement);
+    sectionFiches.appendChild(nomElement);
+    sectionFiches.appendChild(prixElement);
+    sectionFiches.appendChild(categorieElement);
+    sectionFiches.appendChild(descriptionItem);
+    sectionFiches.appendChild(availableItem);
+}
